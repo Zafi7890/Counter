e245d3158ef2a33e2582f25d2a5bb3314ef0552e
Huzaifa-Ishaque <technicalhuzaifa00001@gmail.com>
2025-01-27 19:41:07 +0500

    Counter By Huzaifa

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import { useState } from 'react'
+
+const App = () => {
+  const[Count,setCount]=useState(0);
+  const buttonClicked=(e)=>{
+  let Value=e.target.innerHTML;  
+  if(Value=='Increament'){
+    setCount(Count+1)
+  }else if(Value=='Decreament'){
+    setCount(Count-1)
+  }else{
+    setCount(0)
+  }
+  }
+  return (
+    <>
+    <div className="container col-lg-4 p-4 shadow rounded-3 my-5 d-flex flex-column justify-content-center align-items-center">
+      <h1 className="fw-bolder text-center">{Count}</h1>
+      <div className="">
+      <button onClick={buttonClicked} className="btn btn-success mx-2">Increament</button>
+      <button onClick={buttonClicked} className="btn btn-warning mx-2">Reset</button>
+      <button onClick={buttonClicked} className="btn btn-danger mx-2">Decreament</button>
+      </div>
+    </div>
+    </>
+  )
+}
+
+export default App
